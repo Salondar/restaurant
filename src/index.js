2d57124a93718caa8debe6679582ffe91cc05c89
Mavelasse Zacarias <mrvllscbl41@gmail.com>
2024-11-30 10:59:32 +0200

    Import stylesheet

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import Img from './dish.jpg';
-
+import Img from './pizzaria.jpeg';
+import './style.css';
 
 function Content() {
     const contentdiv = document.querySelector('#content');
 
     const header = document.createElement('h1');
-    header.textContent = 'Taste the heart of Africa';
+    header.textContent = 'Slice of Heaven Pizzeria';
     contentdiv.appendChild(header);
-
-    const para = document.createElement('p');
-    para.textContent = 'Embark on a culinary journey through the diverse flavors of Africa';
-    contentdiv.appendChild(para);
 
     const img = new Image();
     img.src = Img;
+    img.width = 880;
+    img.height = 600;
     contentdiv.appendChild(img);
+
+    const para = document.createElement('p');
+    para.textContent = 'At The Slice of Heaven Pizzeria, we craft mouthwatering, artfully inspired pizzas with the freshest ingredients, served in a vibrant and welcoming atmosphere. Whether you are craving bold flavors or classic comforts, each bite is a little taste of paradise';
+    contentdiv.appendChild(para);
+
 }
 Content();
